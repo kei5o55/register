1a702e3c74e7db2d8620b9f4705ac84f24bb2a89
kei5o55 <uttk2003@gmail.com>
2026-04-14 01:47:26 +0900

    pwa用追加 (#26)
    pwa用にconfigとiconなどいじりました

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,84 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
-/*/ vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // VueならVueのプラグイン
-import { VitePWA } from 'vite-plugin-pwa'
-
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate', // Service Workerを自動更新
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: false,
+      },
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "masked-icon.svg",
+      ],
       manifest: {
-        name: 'マイアプリ名',
-        short_name: 'アプリ',
-        description: 'アプリの説明文',
-        theme_color: '#ffffff',
-        icons: [
+        name: "register",
+        short_name: "regi",
+        description: "My Vite React PWA",
+        theme_color: "#111111",
+        background_color: "#111111",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        "icons": [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            "src": "icons/icon-48x48.png",
+            "sizes": "48x48",
+            "type": "image/png"
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            "src": "icons/icon-72x72.png",
+            "sizes": "72x72",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-96x96.png",
+            "sizes": "96x96",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-128x128.png",
+            "sizes": "128x128",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-152x152.png",
+            "sizes": "152x152",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
           }
         ],
-        display: 'standalone', // これでブラウザのUI（URLバー等）が消えます
-        orientation: 'portrait', // 画面を縦方向に固定したい場合
-        background_color: '#ffffff', // 起動時のスプラッシュ画面の背景色
-        theme_color: '#000000', // スマホの上部ステータスバーの色
-      }
-    })
-  ]
-})*/
+      },
+    }),
+  ],
+});
+
